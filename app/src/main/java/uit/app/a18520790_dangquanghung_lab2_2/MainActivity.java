3c6050d6e52574851c642d4ae59ca9058043cbf9
HungDangQuang <hungdangkg@gmail.com>
2022-04-02 10:02:26 +0700

    sumbit lab 2 ex2

---
+++ app/src/main/java/uit/app/a18520790_dangquanghung_lab2_2/MainActivity.java
@@ -0,0 +1,42 @@
+package uit.app.a18520790_dangquanghung_lab2_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @SuppressLint("ResourceAsColor")
+    public void onRadioClicked(View view){
+        boolean checked = ((RadioButton) view).isChecked();
+
+        if (checked){
+            View someView = findViewById(R.id.frameLayout);
+            switch (view.getId()){
+                case R.id.red:
+                    someView.setBackgroundColor(ContextCompat.getColor(this, R.color.red));
+                    break;
+                case R.id.green:
+                    someView.setBackgroundColor(ContextCompat.getColor(this,R.color.green));
+                    break;
+                case R.id.blue:
+                    someView.setBackgroundColor(ContextCompat.getColor(this,R.color.blue));
+                    break;
+                case R.id.gray:
+                    someView.setBackgroundColor(ContextCompat.getColor(this,R.color.gray));
+                    break;
+            }
+        }
+    }
+}
